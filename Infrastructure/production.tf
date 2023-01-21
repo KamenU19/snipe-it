d5b385bd0c68439cd8f021d59c40985bfb67e081
Kamen Blagoev <kamen.blagoev@officernd.com>
2023-01-22 01:51:32 +0200

    test

---
+++ Infrastructure/production.tf
@@ -0,0 +1,20 @@
+resource "random_id" "random_id_prefix" {
+  byte_length = 2
+}
+
+locals {
+  production_availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
+}
+
+module "Networking" {
+  source               = "./modules/Networking"
+  region               = var.region
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  availability_zones   = local.production_availability_zones
+
+}
+
+
+##
+##
