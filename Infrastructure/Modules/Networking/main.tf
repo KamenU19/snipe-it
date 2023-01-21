0a25d2c91568bd606e0b6c9b67b1f3ec69e0890c
Kamen Blagoev <kamen.blagoev@officernd.com>
2023-01-22 01:15:14 +0200

    Subnet test

--- Infrastructure/Modules/Networking/main.tf
+++ Infrastructure/Modules/Networking/main.tf
@@ -1,1 +1,19 @@
-1
+/*Reading data of the existing VPC*/
+data "aws_vpc" "SnipeIT VPC" {
+  
+  filter {
+    name = "tag:Name"
+    values = ["SnipeIT VPC"]
+  }
+}
+
+/*==== Subnets ======*/
+/* Internet gateway for the public subnet */
+
+resource "aws_internet_gateway" "ig" {
+    vpc_id = data.aws_vpc.SnipeIT VPC.id
+    tags = {
+        Name = "${var.environment}-igw"
+        Environment = var.environment
+    }
+}
