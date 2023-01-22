299c1d6971b14121bee5f3cd876c96411aab198c
Kamen Blagoev <kamen.blagoev@officernd.com>
2023-01-22 02:37:28 +0200

    Adding the subnets

--- Infrastructure/Modules/Networking/main.tf
+++ Infrastructure/Modules/Networking/main.tf
@@ -7,7 +7,7 @@ data "aws_vpc" "snipeitvpc" {
   }
 }
 
-/*==== Subnets7 ======*/
+/*==== Subnets ======*/
 /* Internet gateway for the public subnet */
 
 resource "aws_internet_gateway" "ig" {
@@ -16,4 +16,30 @@ resource "aws_internet_gateway" "ig" {
         Name = "${var.environment}-igw"
         Environment = var.environment
     }
+}
+
+#Public subnet 
+resource "aws_subnet" "public_subnet" {
+    vpc_id = "${data.aws_vpc.snipeitvpc.id}"
+    count = "${length(var.public_subnets_cidr)}"
+    cidr_block = "${element(var.public_subnets_cidr,   count.index)}"
+    availability_zone = "${element(var.availability_zones,   count.index)}"
+    map_public_ip_on_launch = true
+    tags= {
+        Name = "${var.environment}-${element(var.availability_zones, count.index)}-public-subnet"
+        Environment =  "${var.environment}"
+    }
+}
+
+#Private subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id = "${data.aws_vpc.snipeitvpc.id}"
+  count = "${length(var.private_subnets_cidr)}"
+  cidr_block = "${element(var.private_subnets_cidr, count.index)}"
+  availability_zone =  "${element(var.availability_zones,   count.index)}"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${var.environment}-${element(var.availability_zones, count.index)}-private-subnet"
+    Environment = "${var.environment}"
+  }
 }
--- Infrastructure/Modules/Networking/output.tf
+++ Infrastructure/Modules/Networking/output.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
   value = data.aws_vpc.snipeitvpc.id
 }
+
+output "public_subnets_id" {
+  value = ["${aws_subnet.public_subnet.*.id}"]
+}
+
+output "private_subnets_id" {
+  value = ["${aws_subnet.private_subnet.*.id}"]
+}
