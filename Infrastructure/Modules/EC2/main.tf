8f1a6701ab2cbce9ea5365ed01e6fb3872cccbbc
Kamen Blagoev <kamen.blagoev@officernd.com>
2023-01-22 13:23:56 +0200

    adding tag to server

--- Infrastructure/Modules/EC2/main.tf
+++ Infrastructure/Modules/EC2/main.tf
@@ -1,5 +1,6 @@
 #Create the EC2 machine
 resource "aws_instance" "snipeitserver" {
+  tags = { Name = "SnipeIT Server" } 
   ami = var.ami
   instance_type = var.ec2_instance_type
   subnet_id = var.subnets_id[0]
