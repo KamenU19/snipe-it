657e4532e2ec0a6bb7a7e1d377d8d5c588033323
Kamen Blagoev <kamen.blagoev@officernd.com>
2023-01-22 14:45:19 +0200

    test

--- Infrastructure/Modules/githubrunner/main.tf
+++ Infrastructure/Modules/githubrunner/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "runner" {
-    tags = { Name = "Github Runner1" } 
+    tags = { Name = "Github Runner2" } 
     ami = var.ami
     instance_type = var.ec2_instance_type
     subnet_id = var.subnets_id[0]
     vpc_security_group_ids = var.sec_groups
+    key_name = “Kamen.pem”
     
 user_data = <<EOF
 #!/bin/bash
