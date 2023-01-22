321b202c7789ef1287d96b8a339950bb664310b6
Kamen Blagoev <kamen.blagoev@officernd.com>
2023-01-22 15:46:22 +0200

    fixing eroors

--- Infrastructure/Modules/S3/main.tf
+++ Infrastructure/Modules/S3/main.tf
@@ -1,13 +1,9 @@
-provider "aws" {
-  region = var.region
-}
-
 resource "aws_s3_bucket" "state-store" {
   bucket        = var.bucket-name
   acl           = "private"
   force_destroy = var.force_destroy
 
-  server_side_encryption_configuration {
+server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
