5fea771b1119f0c6cfab0a8e4c54638c118e3cc0
Kamen Blagoev <kamen.blagoev@officernd.com>
2023-01-23 14:16:03 +0200

    Deploying the app test

---
+++ app/dockerfiles/file.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+sudo chown ubuntu:ubuntu /home/ubuntu/snipeit
+sudo mv /tmp/docker-compose.yml /home/ubuntu/snipeit/docker-compose.yml
+sudo mv /tmp/my_env_file /home/ubuntu/snipeit/my_env_file
+sudo docker-compose down
+echo "Waiting 30 seconds"
+sleep 30s
+sudo docker-compose up -d
