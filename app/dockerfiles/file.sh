9bedf749617e76fd8d22d4676b66e67a4ee8cf20
Kamen Blagoev <kamen.blagoev@officernd.com>
2023-01-23 14:39:16 +0200

    fixing script path

--- app/dockerfiles/file.sh
+++ app/dockerfiles/file.sh
@@ -2,6 +2,7 @@
 sudo chown ubuntu:ubuntu /home/ubuntu/snipeit
 sudo mv /tmp/docker-compose.yml /home/ubuntu/snipeit/docker-compose.yml
 sudo mv /tmp/my_env_file /home/ubuntu/snipeit/my_env_file
+cd /home/ubuntu/snipeit
 sudo docker-compose down
 echo "Waiting 30 seconds"
 sleep 30s
