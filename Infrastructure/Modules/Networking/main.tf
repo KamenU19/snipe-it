8d8ecf2a5e9c7826d98cfb917d3e6bbccee7698b
Kamen Blagoev <kamen.blagoev@officernd.com>
2023-01-24 14:19:42 +0200

    Fix DB security group

--- Infrastructure/Modules/Networking/main.tf
+++ Infrastructure/Modules/Networking/main.tf
@@ -127,26 +127,6 @@ resource "aws_security_group_rule" "public-non-secure-internet-egress" {
   security_group_id = aws_security_group.snipeit.id
 }
 
-#----- allow database 3306 port
-
-resource "aws_security_group_rule" "database-ingress" {
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  cidr_blocks       = ["10.0.0.0/16"]
-  security_group_id = aws_security_group.snipeit.id
-}
-
-resource "aws_security_group_rule" "database-egress" {
-  type              = "egress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  cidr_blocks       = ["10.0.0.0/16"]
-  security_group_id = aws_security_group.snipeit.id
-}
-
 #----- allow home ip
 
 resource "aws_security_group_rule" "ssh-home-ingress" {
@@ -180,6 +160,31 @@ resource "aws_security_group_rule" "ssh-internal-egress" {
   type              = "egress"
   from_port         = 22
   to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/16"]
+  security_group_id = aws_security_group.snipeit.id
+}
+
+resource "aws_security_group" "RDSsecgroup" {
+  vpc_id = data.aws_vpc.snipeitvpc.id
+  name = "snipeit"
+}
+
+#----- allow database 3306 port
+
+resource "aws_security_group_rule" "database-ingress" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/16"]
+  security_group_id = aws_security_group.snipeit.id
+}
+
+resource "aws_security_group_rule" "database-egress" {
+  type              = "egress"
+  from_port         = 3306
+  to_port           = 3306
   protocol          = "tcp"
   cidr_blocks       = ["10.0.0.0/16"]
   security_group_id = aws_security_group.snipeit.id
