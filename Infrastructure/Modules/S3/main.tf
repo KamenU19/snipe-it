437075d43658b43182ad89e3cdfbb25629e9801d
Kamen Blagoev <kamen.blagoev@officernd.com>
2023-01-24 16:33:35 +0200

    test

--- Infrastructure/Modules/S3/main.tf
+++ Infrastructure/Modules/S3/main.tf
@@ -6,7 +6,8 @@ resource "aws_s3_bucket" "snipeitterrastate" {
 server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+           kms_master_key_id = aws_kms_key.mykey.arn
+           sse_algorithm     = "aws:kms"
       }
     }
   }
